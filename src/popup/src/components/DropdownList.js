a9bd32f529b3031ec070b283e288769fb8125034
CazamirTeodor <cazamirteodor@gmail.com>
2022-02-12 15:41:08 +0200

    Added dropdown component

--- src/popup/src/components/DropdownList.js
+++ src/popup/src/components/DropdownList.js
@@ -1,5 +1,48 @@
 import React from "react";
+import DropdownIcon from '../assets/dropdown-icon.png';
+import '../styles/DropdownList.css';
 
-class DropdownList extends DropdownList {
-    
+class DropdownList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            show: false,
+            selectedOption: ''
+        };
+    }
+
+    setOption = option => {
+        this.setState({
+            show: false,
+            selectedOption : option
+        })
+    }
+
+    toggleSelect = _ => {
+        this.setState({
+            show: !this.state.show,
+        });
+    }
+
+
+    render() {
+        return (
+            <div className="dropdown" style={{overflow: this.state.show?"visible":"hidden"}} onClick={this.toggleSelect}>
+                <div className="chosen-option">
+                    <p>{this.state.selectedOption}</p>
+                </div>
+                <div className="all-options">
+                {
+                    this.props.options.map((option) => {
+                        return <p className="option" onClick={ () => this.setOption(option)}>{option}</p>
+                    })
+                }
+                </div>
+                <img src={DropdownIcon} className="dropdown-icon"  alt="dropdown-icon" />
+            </div>
+        );
+    }
 }
+
+export default DropdownList;
--- src/popup/src/components/pages/HistoryPage.js
+++ src/popup/src/components/pages/HistoryPage.js
@@ -3,6 +3,7 @@ import BackButton from "../BackButton";
 import BrowsingIcon from "../../assets/browsing-icon.png";
 import DownloadIcon from "../../assets/download-icon.png";
 import '../../styles/HistoryPage.css';
+import DropdownList from "../DropdownList";
 class HistoryPage extends React.Component {
     constructor(props) {
         super(props);
@@ -23,23 +24,34 @@ class HistoryPage extends React.Component {
     }
 
     render() {
+        var options = [
+            'Last week',
+            'Last month',
+            'Last year',
+            'Last 5Y',
+            'Last 10Y',
+            'Last 20Y',
+            'Last 30Y'
+        ];
         return (
             <div className="page historyPage">
                 <BackButton route="/settings" />
                 <p className="Title">HISTORY</p>
                 <div className="row">
                     <div className="column">
+                        <p className="option Title">BROWSING</p>
                         <div className="optionBtn browsing" style={{backgroundColor: this.state.browsing?"#AFF8CE":"#F8AFAF"}}>
                             <img className="optionBtnIcon" src={BrowsingIcon} alt="optionIcon" />
                         </div>
-                        <div className="dropdown"></div>
+                        <DropdownList options={options}/>
                     </div>
                     <div className="separator"></div>
                     <div className="column">
+                        <p className="option Title">DOWNLOADS</p>
                         <div className="optionBtn download" style={{backgroundColor: this.state.download?"#AFF8CE":"#F8AFAF"}}>
                             <img className="optionBtnIcon" src={DownloadIcon} alt="optionIcon" />
                         </div>
-                        <div className="dropdown"></div>
+                        <DropdownList options={options}/>
                     </div>
                 </div>
             </div>
