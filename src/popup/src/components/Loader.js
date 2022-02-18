dc47f62a689df912962628b00babb90a17d063f5
CazamirTeodor <cazamirteodor@gmail.com>
2022-02-18 18:52:45 +0200

    Modified loader

--- src/popup/src/components/Loader.js
+++ src/popup/src/components/Loader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from '../assets/logo_fill_w.png';
+import Logo from '../assets/logo_fill_blue.png';
 import '../styles/Loader.css';
 
 class Loader extends React.Component {
